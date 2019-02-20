b1ab193ca3d7f179d56001653572c29c4afa82df
Matthew Tse <tsemd@bu.edu>
2019-02-20 15:34:36 -0500

    updated to reflect BU smtp

--- pmsapp/__init__.py
+++ pmsapp/__init__.py
@@ -14,11 +14,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_SERVER'] = 'smtp.bu.edu'
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+# Must Apply OS username/Password
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
 mail = Mail(app)
 migrate = Migrate(app, db)
 
