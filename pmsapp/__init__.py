e523e69e802db9f2514265bd6a15afd3f6fa8217
matttse <tsemd@bu.edu>
2019-03-04 18:34:31 -0500

    updated to point to persistent database

--- pmsapp/__init__.py
+++ pmsapp/__init__.py
@@ -5,8 +5,13 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 
 application = Flask(__name__)
+# Secret key required always
 application.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# Local Database
+# application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# AWS RDS Database
+application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{user}:{password}@{server}/{database}'.format(user='admin', password='#Y_)~-s]LB2t', server='projectmanagementservicetest.cjkwv9ccpled.us-east-1.rds.amazonaws.com:3306', database='projectdb')
+
 db = SQLAlchemy(application)
 bcrypt = Bcrypt(application)
 login_manager = LoginManager(application)
