a5d213d8140100a2ac8b6d8eb2e2d0eb129e11e4
matttse <tsemd@bu.edu>
2019-03-04 15:44:42 -0500

    connection info

--- pmsapp/__init__.py
+++ pmsapp/__init__.py
@@ -5,19 +5,18 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# UNCOMMENT THIS TO RUN LOCALDB for development
+# app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# AWS config from object
+app.config.from_object('config')
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 migrate = Migrate(app, db)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-#create all db tables
-@app.before_first_request
-def create_tables():
-    from pmsapp import models
-    db.create_all()
-
 
 from pmsapp import routes
