143eb74a1138e39f2084321127b532b8c367f23a
Matthew Tse <tsemd@bu.edu>
2019-03-29 09:34:57 -0400

    using SelectField on WTForms

--- service_application_package/issues/forms.py
+++ service_application_package/issues/forms.py
@@ -1,14 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, DateField, validators
+from wtforms import StringField, SubmitField, TextAreaField, DateField, validators, SelectField
 from wtforms.validators import DataRequired
-from wtforms_sqlalchemy.fields import QuerySelectField
 from service_application_package.models import Project, User
-
-def getUsers():
-	return User.query.all()
-
-def project_ids():
-	return Project.query.all()
 
 class IssueForm(FlaskForm):
     title = StringField('Issue Title', validators=[DataRequired()])
@@ -16,6 +9,11 @@ class IssueForm(FlaskForm):
     issue_date = DateField(u'date create')
     priority = TextAreaField('Priority', validators=[DataRequired()])
     completed_date = DateField('Completed Date', format='%m/%d/%Y', validators=(validators.Optional(),))
-    project = QuerySelectField("ProjectID", query_factory=project_ids, allow_blank=False)
+    opened_by = SelectField('Opened By', validators=[DataRequired()])
+    project = SelectField('Project Name', validators=[DataRequired()])
     submit = SubmitField('Submit New Issue')
 
+    def __init__(self, *args, **kwargs):
+        super(IssueForm, self).__init__(*args, **kwargs)
+        self.project.choices = [(a.id, a.title) for a in Project.query.order_by(Project.title)]
+        self.opened_by.choices = [(a.id, a.username) for a in User.query.order_by(User.username)]
