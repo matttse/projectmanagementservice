fd764e93052705038df519936f9496cac92b8d47
Matthew D Tse <tsemd@bu.edu>
2019-03-31 13:58:28 -0400

    removed unused route

--- service_application_package/chat/routes.py
+++ service_application_package/chat/routes.py
@@ -1,7 +1,6 @@
 from flask import (render_template, url_for, flash,
                    redirect, request, abort, Blueprint)
 from flask_login import current_user, login_required
-from service_application_package.chat.forms import ChatForm
 from gevent import monkey
 monkey.patch_all()
 from flask import session
