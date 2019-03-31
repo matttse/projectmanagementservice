fb11aefeb3a8dbc7da4338038f5768a3c6584669
Matthew D Tse <tsemd@bu.edu>
2019-03-31 15:39:14 -0400

    init app from main

--- service_application_package/__init__.py
+++ service_application_package/__init__.py
@@ -4,9 +4,6 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from service_application_package.config import Config
-from service_application_package.chat import chatsocket
-import redis
-myredis = redis.Redis(host='localhost',port=6379,db=0)
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
