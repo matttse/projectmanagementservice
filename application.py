f2d677e6a0f6b11ab157ac3237e66a8627926e35
Matthew D Tse <tsemd@bu.edu>
2019-04-03 18:04:39 -0400

    Revert "Updates/chat merge"

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 from service_application_package import create_app
+from service_application_package.chat.chatroute import socketio
 application = create_app()
 
-if __name__ == '__main__':	
+if __name__ == '__main__':
+	socketio.init_app(application)
 	application.run(debug=True)
