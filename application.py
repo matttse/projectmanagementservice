92f2a7185cb00eeb78bf69cc71024274dca5d869
yp0406 <yp19960406@gmail.com>
2019-04-03 16:59:40 -0400

    local1.0

--- application.py
+++ application.py
@@ -4,5 +4,5 @@ application = create_app()
 
 if __name__ == '__main__':
 	socketio.init_app(application)
-	socketio.run(application, host='master.projectchat.rpwclh.use1.cache.amazonaws.com', port=5000)
+	socketio.run(application, host='127.0.0.1', port=5000)
 	application.run(debug=True)
