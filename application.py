86caf74155ff8cd21aa507187a902a612f2ee7b8
yp0406 <yp19960406@gmail.com>
2019-04-11 10:50:51 -0400

    make it better

--- application.py
+++ application.py
@@ -4,4 +4,4 @@ application = create_app()
 
 if __name__ == '__main__':
 	socketio.init_app(application)
-	socketio.run(application,host='0.0.0.0', port=5000)
+	socketio.run(application,host='0.0.0.0', port=49276)
