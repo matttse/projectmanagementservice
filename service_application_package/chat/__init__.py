d0f112afd1395c971c7d05c2fa7c248cbb12ef5e
tony <ht6100@gmail.com>
2019-04-17 09:49:28 +0800

    删除不用的文件

--- service_application_package/chat/__init__.py
+++ service_application_package/chat/__init__.py
@@ -1,14 +1,3 @@
 from flask import Flask
 import redis
-import redis
-import os
-
-
-url = os.environ.get('REDISCLOUD_URL')
-port = os.environ.get('REDIS_PORT')
-password = os.environ.get('REDIS_PW')
-
-
-myredis = redis.Redis(host=url,port=port,password=password,ssl=True)
-# myredis = redis.Redis(host='localhost',port=6379,db=0)
-#myredis = redis.Redis(host='localhost',port=6379,db=0)
+myredis = redis.Redis(host='localhost',port=6379,db=0)
--- service_application_package/config.py
+++ service_application_package/config.py
@@ -4,7 +4,8 @@ from datetime import timedelta
 class Config:
     #SECRET_KEY = os.environ.get('SECRET_KEY')
     SECRET_KEY='5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = [ 'mysql+pymysql://admin:projecttesting1@projectmanagementservice.cjkwv9ccpled.us-east-1.rds.amazonaws.com:3306/projectdb', True]
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:projecttesting1@projectmanagementservice.cjkwv9ccpled.us-east-1.rds.amazonaws.com:3306/projectdb'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
