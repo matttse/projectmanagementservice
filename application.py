184710db04ffe032957536b8275aa327f423b409
PengYuan <yp19960406@gmail.com>
2019-04-19 22:14:29 +0800

    bind socket to application

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from service_application_package import create_app
 from service_application_package.chat.chatroute import socketio
 application = create_app()
+socketio.init_app(application)
 
 if __name__ == '__main__':
     socketio.run(application)
