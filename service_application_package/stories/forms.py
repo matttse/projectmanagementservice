6592cc36ef595701d00b00efac8ab2f2a097a07d
Matthew Tse <tsemd@bu.edu>
2019-04-29 09:42:52 -0400

    not int

--- service_application_package/stories/forms.py
+++ service_application_package/stories/forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired
 class StoryForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     status = SelectField('Status', choices=[('new','New'), ('in-progress', 'In-Progress'), ('done', 'Done')], validators=[DataRequired()])
-    assigned_to = SelectField('Assigned To', validators=[DataRequired()], coerce=int)
+    assigned_to = SelectField('Assigned To', validators=[DataRequired()])
     content = TextAreaField('Description', validators=[DataRequired()])
     submit = SubmitField('Submit New Story')
  	def __init__(self, *args, **kwargs):
