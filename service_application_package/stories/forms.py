8c602102bd8a2c6b3e6f3b0933daf7a5ba8e8d79
Matthew Tse <tsemd@bu.edu>
2019-04-29 09:40:14 -0400

    updated to show from user choices on stories

--- service_application_package/stories/forms.py
+++ service_application_package/stories/forms.py
@@ -1,10 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, SelectField
+from service_application_package.models import User
 from wtforms.validators import DataRequired
 
 class StoryForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     status = SelectField('Status', choices=[('new','New'), ('in-progress', 'In-Progress'), ('done', 'Done')], validators=[DataRequired()])
-    assigned_to = SelectField('Opened By', choices=[('matt','Matt'), ('ryan', 'Ryan'), ('peter', 'Peter'), ('kevin', 'Kevin'), ('peng', 'Peng')],validators=[DataRequired()])
+    assigned_to = SelectField('Assigned To', validators=[DataRequired()], coerce=int)
     content = TextAreaField('Description', validators=[DataRequired()])
     submit = SubmitField('Submit New Story')
+ 	def __init__(self, *args, **kwargs):
+        super(StoryForm, self).__init__(*args, **kwargs)
+        self.assigned_to.choices = [(a.id, a.username) for a in User.query.order_by(User.username)]
